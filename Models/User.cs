24e644680a13fb2a33cbd49c92bb441a9dc82839
hellmidwi48@gmail.com <hellmidwi48@gmail.com>
2021-11-29 22:21:05 +0700

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace absensionline.Models
+{
+    public class User{
+        public int ID{get;set;}
+        public string Email{get;set;}
+        public string Password{get;set;}
+        public string Massage{get;set;}
+    }
+}//end namespace
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace absensionline.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet(){
+            if (!User.Identity.IsAuthenticated)
+            {
+                return new RedirectToPageResult("/Login");
+            }
+            return Page();
+        }
+    }
+}
+        
